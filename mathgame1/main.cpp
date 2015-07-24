46c88bb51a7233334a0efa70f8dbf365220c2d98
Melvincat <nichildo@gmail.com>
2015-07-25 08:25:19 +1000

    Initial Commit

---
+++ mathgame1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mathgame1
+//
+//  Created by Nicholas on 25/07/2015.
+//  Copyright (c) 2015 Nicholas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
