dba11599be6422d72f2383b92e8ba78641363a25
Melvincat <nichildo@gmail.com>
2015-07-25 12:45:15 +1000

    header for checker

---
+++ mathgame1/Checker.h
@@ -0,0 +1,13 @@
+//
+//  Checker.h
+//  mathgame1
+//
+//  Created by Nicholas on 25/07/2015.
+//  Copyright (c) 2015 Nicholas. All rights reserved.
+//
+
+#ifndef __mathgame1__Checker__
+#define __mathgame1__Checker__
+
+bool Checker(struct QuestionAnswer QA);
+#endif /* defined(__mathgame1__Checker__) */
