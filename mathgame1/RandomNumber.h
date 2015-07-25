800234cce95d76a1f099aacaa8abd4b29e1c1265
Melvincat <nichildo@gmail.com>
2015-07-25 12:08:29 +1000

    header file for random number

---
+++ mathgame1/RandomNumber.h
@@ -0,0 +1,15 @@
+//
+//  RandomNumber.h
+//  mathgame1
+//
+//  Created by Nicholas on 25/07/2015.
+//  Copyright (c) 2015 Nicholas. All rights reserved.
+//
+
+#ifndef __mathgame1__RandomNumber__
+#define __mathgame1__RandomNumber__
+
+#include <stdio.h>
+double RandomNumber(int min, int max);
+
+#endif /* defined(__mathgame1__RandomNumber__) */
