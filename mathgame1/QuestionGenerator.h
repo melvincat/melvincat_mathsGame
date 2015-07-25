79446c8d3164a5999a0f4765a41f25b3761ff73b
Melvincat <nichildo@gmail.com>
2015-07-25 11:40:59 +1000

    header for question generator

---
+++ mathgame1/QuestionGenerator.h
@@ -0,0 +1,14 @@
+//
+//  QuestionGenerator.h
+//  mathgame1
+//
+//  Created by Nicholas on 25/07/2015.
+//  Copyright (c) 2015 Nicholas. All rights reserved.
+//
+
+#ifndef __mathgame1__QuestionGenerator__
+#define __mathgame1__QuestionGenerator__
+
+#include <stdio.h>
+struct QuestionAnswer QuestionGenerator();
+#endif /* defined(__mathgame1__QuestionGenerator__) */
