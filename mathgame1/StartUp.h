834bdd24f023abca74a7f416beda069313c67acf
Melvincat <nichildo@gmail.com>
2015-07-25 10:30:16 +1000

    see last comment

---
+++ mathgame1/StartUp.h
@@ -0,0 +1,23 @@
+//
+//  StartUp.h
+//  mathgame1
+//
+//  Created by Nicholas on 25/07/2015.
+//  Copyright (c) 2015 Nicholas. All rights reserved.
+//
+
+#ifndef __mathgame1__StartUp__
+#define __mathgame1__StartUp__
+void StartUp();
+#endif /* defined(__mathgame1__StartUp__) */
+
+#ifndef _STARTUP_SETUESRNAME_
+#define _STARTUP_SETUESRNAME_
+#include <iostream>
+void setUserName(std::string x);
+#endif
+#ifndef _STARTUP_SETUESRNAME_
+#define _STARTUP_SETUESRNAME_
+#include <iostream>
+std::string getUserName();
+#endif
