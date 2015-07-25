bc3ac73eb2bc215462550e8316b1353eb5742482
Melvincat <nichildo@gmail.com>
2015-07-25 12:07:54 +1000

    struct used to move multiple variables around

---
+++ mathgame1/QuestionAnswer.h
@@ -0,0 +1,17 @@
+//
+//  QuestionAnswer.h
+//  mathgame1
+//
+//  Created by Nicholas on 25/07/2015.
+//  Copyright (c) 2015 Nicholas. All rights reserved.
+//
+
+#ifndef __mathgame1__QuestionAnswer__
+#define __mathgame1__QuestionAnswer__
+#include <iostream>
+struct QuestionAnswer{
+    std::string question;
+    double answer;
+    double UserAnswer;
+};
+#endif /* defined(__mathgame1__QuestionAnswer__) */
